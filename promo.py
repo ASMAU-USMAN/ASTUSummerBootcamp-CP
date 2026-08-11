e0192086716c330cfc120647aca6ccd6b74d9e67
asmau usman <asmau7925@gmail.com>
2026-08-11 21:05:13 +0300

    Create promo.py

---
+++ promo.py
@@ -0,0 +1,9 @@
+n, q = map(int, input().split())
+p = sorted(map(int, input().split()))
+prefix = [0] * (n + 1)
+for i in range(n):
+    prefix[i + 1] = prefix[i] + p[i]
+for _ in range(q):
+    x, y = map(int, input().split())
+    start = n - x
+    print(prefix[start + y] - prefix[start])
